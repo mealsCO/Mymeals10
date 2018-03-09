2fbc78aa08b29e1d222846f27c1e43cadda29bca
edopore <ediij1994@hotmail.com>
2018-03-08 23:22:33 -0500

    ghjgj

--- app/src/main/java/com/edopore/mymeals10/Splash.java
+++ app/src/main/java/com/edopore/mymeals10/Splash.java
@@ -1,13 +1,34 @@
 package com.edopore.mymeals10;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Splash extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.supportRequestWindowFeature(getWindow().FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent l = new Intent().setClass(Splash.this,MainActivity.class);
+                startActivity(l);
+                finish();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_DELAY);
     }
 }
