f816b68a57c97b722d3b3ee5ca992f39b474d8d2
edopore <ediij1994@hotmail.com>
2018-03-18 16:24:11 -0500

    Se organiza el login, falta loginjava

--- app/src/main/java/com/edopore/mymeals10/Login.java
+++ app/src/main/java/com/edopore/mymeals10/Login.java
@@ -2,6 +2,7 @@ package com.edopore.mymeals10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onIngresarClick(View view) {
+    }
+
+    public void onRegistrarClick(View view) {
     }
 }
--- app/src/main/java/com/edopore/mymeals10/Registro.java
+++ app/src/main/java/com/edopore/mymeals10/Registro.java
@@ -2,6 +2,7 @@ package com.edopore.mymeals10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registro extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+
+    public void onRegistrarClick(View view) {
     }
 }
