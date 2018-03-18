4367ce802c97a13b970a534914a71ede8d439c78
omar3331 <omar.murcia@udea.edu.co>
2018-03-18 18:55:39 -0500

    cambio passwor por textpassword

--- app/src/main/java/com/edopore/mymeals10/MainActivity.java
+++ app/src/main/java/com/edopore/mymeals10/MainActivity.java
@@ -1,8 +1,10 @@
 package com.edopore.mymeals10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +16,26 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
-        //getMenuInflater().inflate(R.menu.main,menu)
+        getMenuInflater().inflate(R.menu.menup,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.reg){
+
+            Intent intent = new Intent(MainActivity.this, Perfil.class);
+            //intent.putExtra("usuario", usuario);
+            //intent.putExtra("contrasena", password);
+            startActivityForResult(intent,369);
+
+        }else if (id == R.id.out){
+
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/edopore/mymeals10/Perfil.java
+++ app/src/main/java/com/edopore/mymeals10/Perfil.java
@@ -1,13 +1,59 @@
 package com.edopore.mymeals10;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class Perfil extends AppCompatActivity {
+
+    EditText eUs, ePas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        ePas = findViewById(R.id.ePas);
+        eUs = findViewById(R.id.eUs);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            eUs.setText(extras.getChar("usuario"));
+            ePas.setText(extras.getChar("contrasena"));
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menus,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.prin){
+
+            Intent intent = new Intent(Perfil.this, MainActivity.class);
+            finish();
+
+        }else if (id == R.id.out){
+
+            Intent intent = new Intent(Perfil.this, Login.class);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+        finish();
+        super.onBackPressed();
+    }
+    
 }
