5d22e9749cfce61830ab55c468c61606724611cf
edopore <ediij1994@hotmail.com>
2018-03-18 18:49:01 -0500

    Falta conectar main con perfil y cerrar sesion

--- app/src/main/java/com/edopore/mymeals10/Login.java
+++ app/src/main/java/com/edopore/mymeals10/Login.java
@@ -1,20 +1,56 @@
 package com.edopore.mymeals10;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+    EditText user, pass;
+    String Usuario, Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        user = findViewById(R.id.eUser);
+        pass = findViewById(R.id.ePass);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("juemadre", "Hola");
+        if (requestCode == 1234 && resultCode == RESULT_OK) {
+            Usuario = data.getStringExtra("USER");
+            Password = data.getStringExtra("PASS");
+            user.setText(Usuario);
+        } else {
+            if (requestCode == 1234 && resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, R.string.noreg, Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     public void onIngresarClick(View view) {
+        if (Usuario.isEmpty() || Password.isEmpty()) {
+            Toast.makeText(this, R.string.logerror2, Toast.LENGTH_SHORT).show();
+        }
+        if (Usuario.equals(user.getText().toString()) && Password.equals(pass.getText().toString())) {
+            Intent l = new Intent().setClass(this, MainActivity.class);
+            startActivityForResult(l, 4321);
+        } else {
+            Toast.makeText(this, R.string.logerror, Toast.LENGTH_SHORT).show();
+        }
+        
     }
 
     public void onRegistrarClick(View view) {
+        Intent l = new Intent().setClass(this, Registro.class);
+        startActivityForResult(l, 1234);
     }
 }
--- app/src/main/java/com/edopore/mymeals10/Registro.java
+++ app/src/main/java/com/edopore/mymeals10/Registro.java
@@ -1,17 +1,50 @@
 package com.edopore.mymeals10;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
-
+    EditText User, Pass, CPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        User = findViewById(R.id.eUser);
+        Pass = findViewById(R.id.ePass);
+        CPass = findViewById(R.id.eCpass);
     }
 
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+        finish();
+        super.onBackPressed();
+    }
     public void onRegistrarClick(View view) {
+        String user, pass, cpass;
+        user = User.getText().toString();
+        pass = Pass.getText().toString();
+        cpass = CPass.getText().toString();
+        if (user.isEmpty()||pass.isEmpty()||cpass.isEmpty()){
+            Toast.makeText(this,R.string.nodata,Toast.LENGTH_SHORT).show();
+        }else{
+            if (pass.equals(cpass)){
+                Intent intent = new Intent();
+                intent.putExtra("USER",user);
+                intent.putExtra("PASS",pass);
+                setResult(RESULT_OK,intent);
+                Toast.makeText(this,R.string.registered,Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            else{
+                Pass.setText("");
+                CPass.setText("");
+                Toast.makeText(this,R.string.nopass,Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
