a7bb58839a6f57bf1778a0582f99c183dc65c480
omar3331 <omar.murcia@udea.edu.co>
2018-04-20 10:51:45 -0500

    login funcional, inicia en main

--- app/src/main/java/com/edopore/mymeals10/Splash.java
+++ app/src/main/java/com/edopore/mymeals10/Splash.java
@@ -22,7 +22,7 @@ public class Splash extends AppCompatActivity {
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-                Intent l = new Intent().setClass(Splash.this,Login.class);
+                Intent l = new Intent().setClass(Splash.this,MainActivity.class);
                 startActivity(l);
                 finish();
             }
